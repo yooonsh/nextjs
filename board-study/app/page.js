0f9399eb33ea74bbfbcc4da21356253d179425b3
yoonsh <yoon_sh@cowave.kr>
2023-04-14 16:03:57 +0900

    mogodb  셋팅

---
+++ board-study/app/page.js
@@ -0,0 +1,9 @@
+import { connectDB } from "@/util/database";
+
+export default async function Home() {
+  const db = (await connectDB).db("board-study");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+
+  return <div>하이</div>;
+}
